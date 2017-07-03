70f873704afc7559ce56ebe3217b9b0d0fbb98db
PyromaX <jeux.pyromaxx@gmail.com>
2017-07-03 16:29:25 +0200

    install FOSUser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new CoreBundle\CoreBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
