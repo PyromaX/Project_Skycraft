0217bb2390765f65f9ba9d9ace71709f53dd8809
PyromaX <jeux.pyromaxx@gmail.com>
2017-09-15 12:24:26 +0200

    update materialize

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,3 +1,5 @@
 Template.layout.helpers({
     pageTitle: function() { return Session.get('pageTitle'); }
 });
+
+accountsUIBootstrap3.setLanguage('fr'); // for French
--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,12 +1,8 @@
 Template.header.helpers({
-    activeRouteClass: function(/* route names */) {
-        var args = Array.prototype.slice.call(arguments, 0);
-        args.pop();
 
-        var active = _.any(args, function(name) {
-            return Router.current() && Router.current().route.getName() === name
-        });
+});
 
-        return active && 'active';
-    }
+$(document).ready(function(){
+    /* affix the navbar after scroll below header */
+    $(".navbar").affix({offset: {top: $("header").outerHeight(true)} });
 });
